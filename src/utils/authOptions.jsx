8e26ab6ea6f25739bb20da82f2565959aa2b2d85
Amancodes26 <amansngh8586@gmail.com>
2025-01-27 19:17:04 +0530

    Add NextAuth integration with MongoDB and create session provider

---
+++ src/utils/authOptions.jsx
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import client from "./lib/db"
+import GoogleProvider from "next-auth/providers/google";
+
+ 
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: MongoDBAdapter(client),
+  providers: [GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET
+  })],
+})
