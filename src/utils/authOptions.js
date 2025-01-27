f70a2ebb8082d0a762062c3fa4574597f6e8bb09
Amancodes26 <amansngh8586@gmail.com>
2025-01-27 20:16:58 +0530

    Refactor authOptions to use default export and update import paths

--- src/utils/authOptions.js
+++ src/utils/authOptions.js
@@ -1,13 +1,15 @@
 import NextAuth from "next-auth"
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import client from "./lib/db"
+import client from "../lib/db"
 import GoogleProvider from "next-auth/providers/google";
 
- 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+const authOptions = {
   adapter: MongoDBAdapter(client),
   providers: [GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET
   })],
-})
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default authOptions;
